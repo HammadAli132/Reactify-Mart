bdc44038b3c80db74312164c0751cf39157c58c1
HammadAli132 <108983041+HammadAli132@users.noreply.github.com>
2024-10-16 14:08:00 +0500

    Partial footer completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+export default function Footer() {
+    return (
+        <footer className="container">
+            <div className="max-width">
+                <div className="footer-border"></div>
+                <div className="footer-lists">
+                    <p>Terms . Privacy Policy</p>
+                    <ul className="footer-list" id="footer-basis-list">
+                        <li className="li-heading">Products </li>
+                        <li>Web Studio </li>
+                        <li>DynamicBox Flex </li>
+                        <li>Programming Forms </li>
+                        <li>Integrations </li>
+                        <li>Command-line </li>
+                    </ul>
+                    <ul className="footer-list">
+                        <li className="li-heading">Resources </li>
+                        <li>Documentation </li>
+                        <li>Tutorails & Guides </li>
+                        <li>Blog </li>
+                        <li>Support Center </li>
+                        <li>Partners </li>
+                    </ul>
+                    <ul className="footer-list">
+                        <li className="li-heading">Company </li>
+                        <li>Home </li>
+                        <li>About us </li>
+                        <li>Company values </li>
+                        <li>Pricing </li>
+                        <li>Privacy-Policy </li>
+                    </ul>
+                    <ul className="footer-list">
+                        <li className="li-heading">Subscribe </li>
+                        <li>Get the lastest news and articles to your inbox every month </li>
+                    </ul>
+                </div>
+                <div className="footer-border"></div>
+            </div>
+
+        </footer>
+    )
+}
