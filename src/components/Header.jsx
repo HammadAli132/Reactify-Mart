50f5579a4c95f658d873e8d84214191d4c3fa968
HammadAli132 <108983041+HammadAli132@users.noreply.github.com>
2024-10-23 17:39:37 +0500

    Login Page Completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,15 +10,15 @@ export default function Header() {
             <span id={styles.siteTitle}> <Link to="/">Reactify Mart</Link> </span>
             <ul id={styles.navMenu}>
                 <li className={styles.li}> <Link to="/">Home</Link> </li>
-                <li className={styles.li}> <Link to="products">Products</Link> </li>
-                <li className={styles.li}> <Link to="about">About Us</Link> </li>
+                <li className={styles.li}> <Link to="/products">Products</Link> </li>
+                <li className={styles.li}> <Link to="/about">About Us</Link> </li>
             </ul>
             <div id={styles.extras}>
-                <Link id={styles.loginBtn} to="login">
+                <Link id={styles.loginBtn} to="/login">
                     <img src={loginLogo} alt="Error" />
                     <span>Login</span>
                 </Link>
-                <Link id={styles.signupBtn} to="signup">
+                <Link id={styles.signupBtn} to="/signup">
                     <img src={signupLogo} alt="Error" />
                     <span>Sign Up</span>
                 </Link>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom"
+import styles from "../styles/login.module.css"
+
 export default function Login() {
     return (
-        <div>Login</div>
+        <section className="container" id={styles.section}>
+            <div className="max-width" id={styles.formDiv}>
+                <p id={styles.formTitle}>Login</p>
+                <form action="#" id={styles.form}>
+                    <label htmlFor="email" className={styles.label}>Email Address
+                        <input type="email" name="email" id={styles.email} />
+                    </label>
+                    <label htmlFor="password" className={styles.label}>Password
+                        <input type="password" name="password" id={styles.password} />
+                    </label>
+                    <button type="submit" className={styles.button} id={styles.loginBtn}>Login</button>
+                    <Link to="/signup" className={styles.button} id={styles.signinBtn}>Sign Up</Link>
+                </form>
+            </div>
+        </section>
     )
 }
